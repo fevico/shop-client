a6b5e0fd51094117f5d247b5d8af2dcaf6054175
fevico <ajayifavour81@gmail.com>
2026-06-08 17:52:07 +0100

    remove unnecessary comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./route/AppRoutes";
-// import { CartProvider } from "@/context/CartContext";
-// import { AuthProvider } from "@/context/AuthContext";
 
 export default function App() {
   return (
     <BrowserRouter>
-      {/* <AuthProvider> */}
-        {/* <CartProvider> */}
           <AppRoutes />
-        {/* </CartProvider> */}
-      {/* </AuthProvider> */}
     </BrowserRouter>
   );
 }
